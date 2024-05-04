d9077fc3c014b6435cd661aa92c2e0c390a8dd83
Alexey Svistunov <xdayx53@gmail.com>
2024-05-04 18:07:04 +0300

    after merging

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,13 @@
 
 
 import './App.css'
-<<<<<<< HEAD
 import AppRoutes from './components/routes/AppRoutes'
-=======
-import Header from './components/header/Header'
-import Hero from './components/hero/Hero'
-import Tech from './components/tech/Tech'
->>>>>>> 30162ee7b107c48b07ae25db5c76d1929a506f04
 
 function App() {
  
   return (
     <>
-<<<<<<< HEAD
      <AppRoutes/>
-=======
-      <Header/>
-      <Hero/>
-      <Tech/>
->>>>>>> 30162ee7b107c48b07ae25db5c76d1929a506f04
     </>
   )
 }
