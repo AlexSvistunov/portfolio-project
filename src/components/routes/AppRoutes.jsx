63b3627af69b6a889504223807fc5ed8c4f197af
Alexey Svistunov <xdayx53@gmail.com>
2024-05-05 13:48:54 +0300

    add animation testPage

--- src/components/routes/AppRoutes.jsx
+++ src/components/routes/AppRoutes.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom"
 import MainPage from "../../pages/MainPage"
 import ProjectsPage from "../../pages/ProjectsPage"
+import AnimationTest from "../../pages/AnimationTest"
 
 
 const AppRoutes = () => {
@@ -9,6 +10,7 @@ const AppRoutes = () => {
       <Routes>
         <Route index element={<MainPage/>}/>
         <Route path="/projects" element={<ProjectsPage/>}/>
+        <Route path="/animation" element={<AnimationTest/>}/>
       </Routes>
     </>
   )
