ce0c1357cdb695381c53d73b6c2fcd92fd59a3bd
Alexey Svistunov <xdayx53@gmail.com>
2024-05-05 14:26:22 +0300

    add bubble animation

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,10 +2,12 @@ import Header from "../components/header/Header";
 import Hero from "../components/hero/Hero";
 import Footer from "../components/footer/Footer";
 import Tools from "../components/tools/Tools";
+import BubbleAnimation from "../components/bubble-animation/BubbleAnimation";
 
 const MainPage = () => {
   return (
     <>
+      <BubbleAnimation/>
       <Header />
       <main className="main">
         <Hero />
